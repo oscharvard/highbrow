a815c6685b5f659f7828f45dd02d48142a99dc32
Reinhard Engels <reinhard.engels@gmail.com>
2012-08-16 17:08:38 -0400

    added crude deep link creation UI

---
+++ js/highbrow.linker.js
@@ -0,0 +1,86 @@
+Highbrow.Linker = this.Highbrow.Linker = function(hb,conf) {
+
+    "use strict";
+
+    if (! (this instanceof Highbrow.Linker)) throw "called Highbrow.Linker constructor as if it were a function: missing 'new'.";
+
+    var jqd = null; // jquery dialog object.
+    var searchCount=0;
+
+    var createLink = function(){
+	// create permalink or deep link.
+	// get the selected section if any.
+	var sid = hb.selectedSection ? hb.selectedSection.id : null;
+	// get the zoom a and z.
+	var spa = Math.round(hb.map.spa);
+	var spz = Math.round(hb.map.spz);
+	// todo: note selection state.
+	return("#"+ (sid ? 'select=' + sid : '')  + '&start=' + spa + '&stop=' +spz);
+    };
+
+    var updateNavState = function(){
+	// on init, set zoom and selection state based on url parameters passed after the "#"
+	var fragment = $.deparam.fragment();
+	var sid = fragment.select ? fragment.select : "";
+	var zid = fragment.zoom   ? fragment.zoom : ""; // could be multiple.
+	var spa = fragment.start ? fragment.start : "";
+	var spz = fragment.stop ? fragment.stop : "";
+	// noteSection ?
+	if ( sid ) {
+	    var s = hb.sectionById[sid];
+	    hb.selectSection(s);
+	}
+	if ( zid ) {
+	    // if zoom id is set, it overrides start and stop.
+	    var z = hb.sectionById[zid];
+	    hb.map.setVisibleRange(z.start,z.stop);
+	} else if ( spa && spz ) {
+	    hb.map.setVisibleRange(spa,spz);
+	}
+    };
+    
+    var init = function(){
+	$("#"+hb.prefix + "showLinker").click(function(e) { show() ; e.preventDefault(); });
+	updateNavState();
+	// re-apply state if the hash parameters are changed.
+	$(window).bind( 'hashchange', function(){ updateNavState();   });
+	initDialog();
+    };
+
+    var show = function(){
+	var link = createLink();
+	jQuery.bbq.pushState(  link  );
+	$(hb.pre('#$prelink')).html(document.location.href);
+	$(hb.pre('#$prelink')).attr('href',document.location.href);
+	jqd.dialog('open');
+	return false;
+    };
+
+    var initDialog = function(){
+
+	var dialogId =  hb.prefix+'linkerDialog';
+	var html ="Deep linking allows you to specify precise zoom and selection states in highbrow for bookmarking and sharing.";
+
+	html += hb.pre('<p>The deep link for your current highbrow state is:</p><p><a href="" id="$prelink"></a></p>');
+
+	html += '<p>The url in your web browser has been updated to reflect this.</p>';
+
+	jqd = $('<div id="' + dialogId + '" class=\"hb_misc\"></div>')
+	.html(html)
+	.dialog({
+		autoOpen: false,
+		title: 'Create Deep Link To Highbrow',
+		height: 300,
+		width: 350,
+		buttons:
+		{
+		    "Close": function()  {
+			$(this).dialog("close");
+		    }
+		}
+	});
+    };
+  
+    init();
+    
+};
